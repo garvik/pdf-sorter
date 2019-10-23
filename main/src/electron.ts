2ad0f796c337dafceaa838e05c5cc669f1b320f7
Michał Cichy <michal.cichy@silvermedia.pl>
2019-10-23 08:09:13 +0200

    initial commit

---
+++ main/src/electron.ts
@@ -0,0 +1,42 @@
+import { BrowserWindow, app, ipcMain, IpcMainEvent } from "electron";
+import * as isDev from "electron-is-dev";
+import * as path from "path";
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  mainWindow.on("closed", () => mainWindow.destroy());
+
+  ipcMain.on("channel", (_event: IpcMainEvent, msg: any[]) => {
+    console.log(msg);
+    mainWindow.webContents.send("response", { title: "mymessage", data: 1 });
+  });
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
